5a6936a0bae8e0d6e5dadb1c3c9991dc3c987c7d
Anthony <anthony.tapin@reseau.eseo.com>
2023-04-12 10:25:43 +0200

    classe CoordonneesTest

--- Falling_Blocks_Project/test/fr/eseo/e3/poo/projet/blox/modele/CoordonneesTest.java
+++ Falling_Blocks_Project/test/fr/eseo/e3/poo/projet/blox/modele/CoordonneesTest.java
@@ -1,4 +1,41 @@
 package fr.eseo.e3.poo.projet.blox.modele;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CoordonneesTest {
+    @Test
+    public void deplacerVersTest() {
+        Coordonnees c = new Coordonnees(0, 0);
+        c.setAbscisse(1);
+        c.setOrdonnee(1);
+        assertEquals(1, c.getAbscisse());
+        assertEquals(1, c.getOrdonnee());
+    }
+    @Test
+    public void deplacerDeTest() {
+        Coordonnees c = new Coordonnees(0, 0);
+        c.setAbscisse(1);
+        c.setOrdonnee(1);
+        assertEquals(1, c.getAbscisse());
+        assertEquals(1, c.getOrdonnee());
+    }
+
+    @Test
+    public void equalsTest() {
+        Coordonnees c1 = new Coordonnees(0, 0);
+        Coordonnees c2 = new Coordonnees(1, 1);
+        assertEquals(false, c1.equals(c2));
+    }
+    @Test
+    public void hashCodeTest() {
+        Coordonnees c1 = new Coordonnees(0, 0);
+        Coordonnees c2 = new Coordonnees(1, 1);
+        assertEquals(1, c1.hashCode());
+    }
+    @Test
+    public void toStringTest() {
+        Coordonnees c1 = new Coordonnees(0, 0);
+        assertEquals("(0,0)", c1.toString());
+    }
 }
