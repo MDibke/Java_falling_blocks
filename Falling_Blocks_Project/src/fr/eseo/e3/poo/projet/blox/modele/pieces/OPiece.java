e3345577eff847b211445f996773430fc5b829c4
Anthony <anthony.tapin@reseau.eseo.com>
2023-04-12 11:19:23 +0200

    correction 2 classe CoordonneesTest et Coordonnees

--- Falling_Blocks_Project/src/fr/eseo/e3/poo/projet/blox/modele/pieces/OPiece.java
+++ Falling_Blocks_Project/src/fr/eseo/e3/poo/projet/blox/modele/pieces/OPiece.java
@@ -1,4 +1,12 @@
 package fr.eseo.e3.poo.projet.blox.modele.pieces;
 
-public class OPiece {
+public class OPiece extends Element {
+
+    public OPiece(coordonnees coordonnees) {
+        super(coordonnees);
+    }
+
+    }
+
+    }
 }
