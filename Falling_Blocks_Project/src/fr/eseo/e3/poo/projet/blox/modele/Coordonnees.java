4fd255faf8dead5bb424ccc59514b619dbfb880d
Anthony <anthony.tapin@reseau.eseo.com>
2023-04-12 09:52:51 +0200

    classe Coordonnees

--- Falling_Blocks_Project/src/fr/eseo/e3/poo/projet/blox/modele/Coordonnees.java
+++ Falling_Blocks_Project/src/fr/eseo/e3/poo/projet/blox/modele/Coordonnees.java
@@ -1,4 +1,38 @@
 package fr.eseo.e3.poo.projet.blox.modele;
 
 public class Coordonnees {
+    private int abscisse;
+    private int ordonnee;
+
+    public Coordonnees(int abscisse, int ordonnee) {
+        this.abscisse = abscisse;
+        this.ordonnee = ordonnee;
+    }
+
+    public int getAbscisse() {
+        return abscisse;
+    }
+
+    public int getOrdonnee() {
+        return ordonnee;
+    }
+
+    public void setAbscisse(int abscisse) {
+        this.abscisse = abscisse;
+    }
+
+    public void setOrdonnee(int ordonnee) {
+        this.ordonnee = ordonnee;
+    }
+    public String toString() {
+        return "(" + this.abscisse + "," + this.ordonnee + ")";
+    }
+
+    public boolean equals(int abscisse, int ordonnee) {
+        return (this.abscisse == abscisse && this.ordonnee == ordonnee);
+    }
+
+    public int hashCode() {
+        return this.abscisse + this.ordonnee;
+    }
 }
